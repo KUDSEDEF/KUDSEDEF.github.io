de6865a980a572d6f5928530f1acf43663ae5866
KUD-Sedef <31809525+KUDSEDEF@users.noreply.github.com>
2017-09-21 19:44:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function closeNav() {
     document.getElementById("mySidenavr").style.width = "0";
     document.getElementById("mySidenavl").style.width = "0";
 } 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideShow");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
