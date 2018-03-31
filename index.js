467aef20bd3307e9abfa0eb5454298de2910340b
KUD-Sedef <31809525+KUDSEDEF@users.noreply.github.com>
2018-03-30 20:59:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,85 @@
-function openNavr() {
-    document.getElementById("mySidenavr").style.width = "250px";
+var retract_status = 1;
+function run() {
+    setTimeout(removeLoaderMobile, 1500);
+    setTimeout(removeLoader, 3000);
 }
-function openNavl() {
-    document.getElementById("mySidenavl").style.width = "250px";
+function removeLoaderMobile() {
+    document.getElementById("loader-mobile-series").style.display = "none";
+    document.getElementById("loader").style.display = "block";
 }
-function closeNav() {
-    document.getElementById("mySidenavr").style.width = "0";
-    document.getElementById("mySidenavl").style.width = "0";
-} 
+function removeLoader() {
+    document.getElementById("loader").style.display = "none";
+}
+function retract() {
+    if (retract_status === 1) {
+        document.getElementById("mobile-menu").style.display = "none";
+        document.getElementById("reretractbutton").style.display = "block";
+        retract_status = 0;
+    } else if (retract_status === 0) {
+        document.getElementById("reretractbutton").style.display = "none";
+        document.getElementById("mobile-menu").style.display = "block";
+        retract_status = 1;
+    }
+}
+run();
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    document.getElementById("mobile-container").style.display = "block";
+    document.getElementById("container").style.display = "none";
+}
+
+function dropdownSM() {
+    document.getElementById("dropdownSM").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
+
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    x[slideIndex-1].style.display = "block"; 
-}
-//Info
-var mInfo = document.getElementById('mInfo');
-var bInfo = document.getElementById("bInfo");
-var span = document.getElementsByClassName("close")[0];
-bInfo.onclick = function() {
-    mInfo.style.display = "block";
-}
-span.onclick = function() {
-    mInfo.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == mInfo) {
-        mInfo.style.display = "none";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-}
-//Gallery
-/*var mGallery = document.getElementById('mGallery');
-var bGallery = document.getElementById("bGallery");
-var span = document.getElementsByClassName("close")[0];
-bGallery.onclick = function() {
-    mGallery.style.display = "block";
-}
-span.onclick = function() {
-    mGallery.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == mGallery) {
-        mGallery.style.display = "none";
-    }
-}*/
-//Members
-var mMembers = document.getElementById('mMembers');
-var bMembers = document.getElementById("bMembers");
-var span = document.getElementsByClassName("close")[0];
-bMembers.onclick = function() {
-    mMembers.style.display = "block";
-}
-span.onclick = function() {
-    mMembers.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == mMembers) {
-        mMembers.style.display = "none";
-    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
 }
